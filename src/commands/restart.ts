483a6f8ca2b7548df2e923375f40c3c0db786551
wchanee <chaneewoo@gmail.com>
2025-01-14 17:37:26 +0800

    commit

--- src/commands/restart.ts
+++ src/commands/restart.ts
@@ -6,7 +6,16 @@ export const commandRestart = async (idChat: number) => {
 	state.fee = 2
 	state.records = []
 
-	return await bot.sendMessage(idChat, `所有数据已经重置`, {
-		parse_mode: 'HTML',
-	})
+	return await bot.sendMessage(
+		idChat,
+		`
+		📢设置收工成功!
+		所有数据已经重置
+		祝您2025顺风顺水顺顺财神
+		明日依旧丝滑
+		`,
+		{
+			parse_mode: 'HTML',
+		}
+	)
 }
