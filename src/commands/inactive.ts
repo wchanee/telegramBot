bd8c4bf1cc2bab9a882469be2f928eaa8c575902
wchanee <chaneewoo@gmail.com>
2025-01-22 15:41:19 +0800

    commit

--- src/commands/inactive.ts
+++ src/commands/inactive.ts
@@ -3,13 +3,16 @@ import { sendMessage } from './__utils'
 export const commandInactive = async ({
 	idChat,
 	idReplyTo,
+	hideReport = true,
 }: {
 	idReplyTo: number
 	idChat: number
+	hideReport?: boolean
 }) => {
 	await sendMessage({
 		idChat,
 		idReplyTo,
 		message: '❌ 请先使用 开工 命令启动记账管理机器人!',
+		hideReport,
 	})
 }
